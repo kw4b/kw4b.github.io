23e1345f10bf73d56ec1ea802daaa0bf38280b2f
kw4b <102009440+kw4b@users.noreply.github.com>
2023-07-05 23:14:27 -0500

    revert test of script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// test
-const faviconEl = document.querySelector('link[rel="icon"]')
-
-
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-mediaQuery.addEventListener('change', themeChange)
-
-
-function themeChange(event) {
-  if (event.matches) {
-    faviconEl.setAttribute('href', 'favicon-dark.png')
-  } else {
-    faviconEl.setAttribute('href', 'favicon-light.png')
-  }
-}
-
 (function () {
   var on = addEventListener,
     off = removeEventListener,
